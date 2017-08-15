f4736f41b9aad91dbb24f67eef3a7beadc5f369a
Oleksandr Bukhanenko <alex.buhanenko@gmail.com>
2017-08-15 22:38:21 +0300

    Registration page fixes (new inputs, input values format, validation fixes)

--- public/js/user/new.js
+++ public/js/user/new.js
@@ -1,70 +1,87 @@
 ﻿
- $( document ).ready( function () {
-
+$( document ).ready( function () {
   $('#registrationForm').validate({
    rules: {
-
-    userName: {
-      namecheck: true,
-      required: true,
-      minlength: 4,
-      maxlength:20
-    },
-    password: {
-      pwcheck: true,
-      required: true,
-      minlength: 6,
-      maxlength: 16
-    },
-    password_confirm: {
-      required: true,
-      minlength: 6,
-      maxlength: 16,
-      equalTo: "#password"
-    },
-    email: {
-      required: true,
-      email: true
+    "user[firstName]": {
+      maxlength:10,
+      kyrylic_namecheck : true
     },
 
+    "user[lastname]": {
+     maxlength:10,
+      kyrylic_namecheck : true
+   },
+
+   "user[username]": {
+    namecheck: true,
+    required: true,
+    minlength: 4,
+    maxlength:20
   },
-  messages: {
-
-    userName: {
-		namecheck: "Имя должно состоять из букв верхнего и нижнего реестра а также цифр",
-      required: "Введите имя пользователя",
-      minlength: "Минимальная длинна имени - 4 символа"
-    },
-    password: {
-      pwcheck: "Пароль должен состоять из букв верхнего и нижнего реестра а также цифр",
-      required: "Придумайте Ваш пароль",
-      minlength: "Ваш пароль сликом короткий"
-
-    },
-    password_confirm: {
-      required: "Повторите пароль",
-      minlength: "Ваш пароль сликом короткий",
-      equalTo: "Пароли не совпадают"
-    },
-    email: "Введите настоящий E-mail-адрес",
-    CheckBoxConfirm: "Приймите условия пользовательского соглашения"
+  "user[password]": {
+    pwcheck: true,
+    required: true,
+    minlength: 6,
+    maxlength: 16
   },
-  errorElement: 'span',
-  errorClass: 'help-block',
-  errorPlacement: function ( error, element ) {
-          error.addClass( "help-block" );
-          element.parents( ".form-group" ).addClass( "has-feedback" );
-          if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
-            error.insertAfter(element.parent());
-          } else {
-            error.insertAfter(element);
-          }
-          if ( !element.next( "span" )[ 0 ] ) {
-            $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
-          }
+  password_confirm: {
+    required: true,
+    minlength: 6,
+    maxlength: 16,
+    equalTo: "#password"
+  },
+  "user[email]": {
+    required: true,
+    email: true
+  },
 
-        },
-        success: function ( label, element ) {
+},
+messages: {
+  "user[firstName]": {
+    required: "Введите свое имя",
+    maxlength: "Максимальная длинна имени - 10 символов",
+    kyrylic_namecheck : "Недопустимые символы"
+  },
+  "user[lastname]": {
+   required: "Введите свою фамилию",
+   maxlength: "Допустимая длинна - 10 символов",
+   kyrylic_namecheck : "Недопустимые символы"
+ },
+
+ "user[username]" : {
+  namecheck: "Имя должно состоять из букв верхнего и нижнего реестра а также цифр",
+  required: "Введите имя пользователя",
+  minlength: "Минимальная длинна имени - 4 символа"
+},
+"user[password]": {
+  pwcheck: "Пароль должен состоять из букв верхнего и нижнего реестра а также цифр",
+  required: "Придумайте Ваш пароль",
+  minlength: "Ваш пароль сликом короткий"
+},
+password_confirm: {
+  required: "Повторите пароль",
+  minlength: "Ваш пароль сликом короткий",
+  equalTo: "Пароли не совпадают"
+},
+"user[email]": "Введите настоящий E-mail-адрес",
+CheckBoxConfirm: "Приймите условия пользовательского соглашения"
+},
+errorElement: 'span',
+errorClass: 'help-block',
+errorPlacement: function ( error, element ) {
+  error.addClass( "help-block" );
+  element.parents( ".form-group" ).addClass( "has-feedback" );
+  if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+    error.insertAfter(element.parent());
+  } else {
+    error.insertAfter(element);
+  }
+  if ( !element.next( "span" )[ 0 ] ) {
+    $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+  }
+
+},
+success: function ( label, element ) {
           // Add the span element, if doesn't exists, and apply the icon classes to it.
           if ( !$( element ).next( "span" )[ 0 ] ) {
             $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
@@ -88,5 +105,8 @@
    return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
        && /[a-z]/.test(value) // has a lowercase letter
       // && /\d/.test(value) // has a digit
+    });
+    $.validator.addMethod("kyrylic_namecheck", function(value) {
+   return /^[А-Яа-яA-Za-z]*$/.test(value) // consists of only these
     });
 } );
