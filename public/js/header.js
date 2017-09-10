101115429f295757661ebcef22c584d537e2c38f
Oleksandr Bukhanenko <alex.buhanenko@gmail.com>
2017-09-10 18:16:36 +0300

    Login/registration page, login modal window, styles fix, int-tel-input integration

---
+++ public/js/header.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.launch-modal').on('click', function(e){
+    e.preventDefault();
+    $( '#' + $(this).data('modal-id') ).modal();
+  });
+})
--- public/js/user/new.js
+++ public/js/user/new.js
@@ -1,54 +1,95 @@
-﻿
- $( document ).ready( function () {
+﻿$( document ).ready( function () {
+var countryData = $.fn.intlTelInput.getCountryData();
+$.each(countryData, function(i, country) {
+  country.name = country.name.replace(/.+\((.+)\)/,"$1");
+});
 
+$("#phone").intlTelInput({
+  onlyCountries: ["ua", "ru", "md", "kz", "by", "ge", "ee", "lt", "lv", "pl" ,"az", "ro", "hu", "au", "br", "tr", "tn", "am"],
+  initialCountry: "auto",
+  geoIpLookup: function(callback) {
+    $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+      var countryCode = (resp && resp.country) ? resp.country : "";
+      callback(countryCode);
+    });
+  },
+     utilsScript: "./../../lib/intl-tel-input/build/js/intlTelInput.js"
+});
   $('#registrationForm').validate({
-   rules: {
+      rules: {
+          "user[firstName]": {
+              maxlength:10,
+              minlength: 3,
+              kyrylic_namecheck : true
+          },
+          "user[lastName]": {
+              maxlength:10,
+              minlength: 3,
+              kyrylic_namecheck : true
+          },
+          "user[userName]": {
+              namecheck: true,
+              required: true,
+              minlength: 4,
+              maxlength:20
+          },
+          "user[password]": {
+              pwcheck: true,
+              required: true,
+              minlength: 6,
+              maxlength: 16
+          },
+          password_confirm: {
+              required: true,
+              minlength: 6,
+              maxlength: 16,
+              equalTo: "#password"
+          },
+          "user[email]": {
+              required: true,
+              email: true
+          },
+         "user[phone]" :{
+              required: true,
+              number: true
+          }
+      },
+      messages: {
+          "user[phone]":{
+              required: "Обязательное поле",
+              number: "Недопустимые символы"
+          },
+          "user[firstName]": {
+              required: "Введите свое имя",
+              maxlength: "Максимальная длинна имени - 10 символов",
+              minlength: "Минимальная длинна имени - 3 символа",
+              kyrylic_namecheck : "Недопустимые символы"
+          },
+          "user[lastName]": {
+              required: "Введите свою фамилию",
+              maxlength: "Допустимая длинна - 10 символов",
+              minlength: "Минимальная длинна имени - 3 символа",
+              kyrylic_namecheck : "Недопустимые символы"
+          },
+          "user[userName]" : {
+              namecheck: "Имя должно состоять из букв верхнего и нижнего реестра а также цифр",
+              required: "Введите имя пользователя",
+              minlength: "Минимальная длинна имени - 4 символа"
+          },
+          "user[password]": {
+              pwcheck: "Пароль должен состоять из букв верхнего и нижнего реестра а также цифр",
+              required: "Придумайте Ваш пароль",
+              minlength: "Ваш пароль сликом короткий"
+          },
+          password_confirm: {
+              required: "Повторите пароль",
+              minlength: "Ваш пароль сликом короткий",
+              equalTo: "Пароли не совпадают"
+          },
+          "user[email]": "Введите настоящий E-mail-адрес",
+          CheckBoxConfirm: "Приймите условия пользовательского соглашения"
+      },
 
-    userName: {
-      namecheck: true,
-      required: true,
-      minlength: 4,
-      maxlength:20
-    },
-    password: {
-      pwcheck: true,
-      required: true,
-      minlength: 6,
-      maxlength: 16
-    },
-    password_confirm: {
-      required: true,
-      minlength: 6,
-      maxlength: 16,
-      equalTo: "#password"
-    },
-    email: {
-      required: true,
-      email: true
-    },
-
-  },
-  messages: {
-
-    userName: {
-		namecheck: "Имя должно состоять из букв верхнего и нижнего реестра а также цифр",
-      required: "Введите имя пользователя",
-      minlength: "Минимальная длинна имени - 4 символа"
-    },
-    password: {
-      pwcheck: "Пароль должен состоять из букв верхнего и нижнего реестра а также цифр",
-      required: "Придумайте Ваш пароль",
-      minlength: "Ваш пароль сликом короткий"
-
-    },
-    password_confirm: {
-      required: "Повторите пароль",
-      minlength: "Ваш пароль сликом короткий",
-      equalTo: "Пароли не совпадают"
-    },
-    email: "Введите настоящий E-mail-адрес",
-    CheckBoxConfirm: "Приймите условия пользовательского соглашения"
-  },
   errorElement: 'span',
   errorClass: 'help-block',
   errorPlacement: function ( error, element ) {
@@ -60,33 +101,29 @@
             error.insertAfter(element);
           }
           if ( !element.next( "span" )[ 0 ] ) {
-            $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+         //   $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
           }
 
         },
-        success: function ( label, element ) {
-          // Add the span element, if doesn't exists, and apply the icon classes to it.
-          if ( !$( element ).next( "span" )[ 0 ] ) {
-            $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
-          }
-        },
+
         highlight: function ( element, errorClass, validClass ) {
           $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
-          $( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
         },
         unhighlight: function ( element, errorClass, validClass ) {
           $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
-          $( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
         }
       } );
-  $.validator.addMethod("pwcheck", function(value) {
-   return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
-       && /[a-z]/.test(value) // has a lowercase letter
-       && /\d/.test(value) // has a digit
+     $.validator.addMethod("pwcheck", function(value) {
+         return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
+             && /[a-z]/.test(value) // has a lowercase letter
+             && /\d/.test(value) // has a digit
      });
-  $.validator.addMethod("namecheck", function(value) {
-   return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
-       && /[a-z]/.test(value) // has a lowercase letter
-      // && /\d/.test(value) // has a digit
-    });
+     $.validator.addMethod("namecheck", function(value) {
+         return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
+             && /[a-z]/.test(value) // has a lowercase letter
+         // && /\d/.test(value) // has a digit
+     });
+     $.validator.addMethod("kyrylic_namecheck", function(value) {
+         return /^[А-Яа-я]*$/.test(value) // consists of only these
+     });
 } );
