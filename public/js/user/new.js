65dc4c750e0bbeb19df016cdfb6b146c1bdaa03f
Oleksandr Bukhanenko <alex.buhanenko@gmail.com>
2017-09-20 22:36:53 +0300

    fixes

--- public/js/user/new.js
+++ public/js/user/new.js
@@ -1,4 +1,4 @@
-﻿$( document ).ready( function () {
+﻿$(document).ready( function () {
   var countryData = $.fn.intlTelInput.getCountryData();
   $.each(countryData, function(i, country) {
     country.name = country.name.replace(/.+\((.+)\)/,"$1");
@@ -15,7 +15,8 @@
     },
     utilsScript: "./../../lib/intl-tel-input/build/js/intlTelInput.js"
   });
-  $('#registrationForm').validate({
+$('form').each(function() {
+  $(this).validate({
     rules: {
       "user[firstName]": {
         maxlength:10,
@@ -43,7 +44,7 @@
         required: true,
         minlength: 6,
         maxlength: 16,
-        equalTo: "#password"
+        equalTo: "#reg_password"
       },
       "user[email]": {
         required: true,
@@ -120,60 +121,5 @@
   $.validator.addMethod("kyrylic_namecheck", function(value) {
          return /^[А-Яа-я]*$/.test(value) // consists of only these
        });
-  $('#loginForm').validate({
-   rules: {
-    "user[userName]": {
-      namecheck: true,
-      required: true,
-      minlength: 4,
-      maxlength:20
-    },
-    "user[password]": {
-      pwcheck: true,
-      required: true,
-      minlength: 6,
-      maxlength: 16
-    }
-  },
-  messages: {
-    "user[userName]": {
-      namecheck: "Недопустимый формат имени",
-      required: "Введите имя пользователя",
-      minlength: "Минимальная длинна имени - 4 символа",
-      maxlength: "Максимальная длинна имени - 20 символов"
-    },
-    "user[password]": {
-      pwcheck: "Недопустимый пароль",
-      required: "Введите пароль",
-      minlength: "Минимальная длинна пароля - 6 символов",
-      maxlength: "Максимальная длинна пароля - 16 символов"
-    }
-  },
-  errorElement: 'span',
-  errorClass: 'help-block',
-  errorPlacement: function ( error, element ) {
-    error.addClass( "help-block" );
-    element.parents( ".form-group" ).addClass( "has-feedback" );
-    if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
-      error.insertAfter(element.parent());
-    } else {
-      error.insertAfter(element);
-    }
-  },
-  highlight: function ( element, errorClass, validClass ) {
-    $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
-  },
-  unhighlight: function ( element, errorClass, validClass ) {
-    $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
-  }
 });
-  $.validator.addMethod("pwcheck", function(value) {
-   return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
-       && /[a-z]/.test(value) // has a lowercase letter
-       && /\d/.test(value) // has a digit
-     });
-  $.validator.addMethod("namecheck", function(value) {
-   return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
-       && /[a-z]/.test(value) // has a lowercase letter
-     });
-} );
+});
